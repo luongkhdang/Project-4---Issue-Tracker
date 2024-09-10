b92bf22aba793c960f7af1c5721795fd2a960104
Luong Dang <luongkhdang@gmail.com>
2024-09-09 18:05:42 -0700

    refactor: organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,7 @@
+import ErrorMessage from "./ErrorMessage";
+import IssueStatusBadge from "./IssueStatusBadge";
+import Link from "./Link";
+import Spinner from "./Spinner";
+
+export { default as Skeleton } from "./Skeleton";
+export { ErrorMessage, IssueStatusBadge, Link, Spinner };
