683d1ed4e209b203ea24cbefc94ac84a68a77058
Luong Dang <luongkhdang@gmail.com>
2024-09-09 19:06:41 -0700

    build an api for updating issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ErrorMessage from "@/app/components/ErrorMessage";
-import createIssueSchema from "@/app/validationSchemas";
+import issueSchema from "@/app/validationSchemas";
 import dynamic from "next/dynamic";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,7 +23,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue: Issue }) => {
   const router = useRouter();
@@ -33,7 +33,7 @@ const IssueForm = ({ issue }: { issue: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
 
   const [error, setError] = useState("");
