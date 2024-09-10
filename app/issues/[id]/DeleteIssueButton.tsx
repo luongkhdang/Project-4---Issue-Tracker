8cdc34dfce7ee3768b6399692e8e3f14e740f565
Luong Dang <luongkhdang@gmail.com>
2024-09-10 16:03:11 -0700

    improve UX

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -9,12 +10,17 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
 
   const [error, setError] = useState(false);
+
+  const [isDeleting, setDeleting] = useState(false);
+
   const deleteIssue = async () => {
     try {
+      setDeleting(true);
       await axios.delete("/api/issues/" + issueId);
       router.push("/issues");
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   };
@@ -23,7 +29,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="ruby">Delete Issue</Button>
+          <Button color="ruby" disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
