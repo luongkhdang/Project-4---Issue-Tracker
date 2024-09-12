5ed6514db6bb3b0ae1408a2d621d0f2476e6a1ca
Luong Dang <luongkhdang@gmail.com>
2024-09-12 14:34:01 -0700

    fetch data with react query (tanstack)

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "@radix-ui/themes/styles.css";
 import "./globals.css";
 
 import AuthProvider from "./auth/Provider";
+import QueryClientProvider from "./QueryClientProvider";
 import "./theme-config.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -24,16 +25,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
-      <AuthProvider>
-        <body className={inter.className}>
-          <Theme accentColor="red" radius="large" scaling="110%">
-            <NavBar />
-            <main className="p-5">
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </body>
-      </AuthProvider>
+      <body className={inter.className}>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="red" radius="large" scaling="110%">
+              <NavBar />
+              <main className="p-5">
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
